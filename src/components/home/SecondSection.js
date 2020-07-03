c83bfd7a8699cdc902ebe6de1bc91ac46bc2ee84
Ariel  Molina <itzli2000@msn.com>
2020-07-02 21:39:30 -0500

    Feature Working with second section of home page Added resume files for english and spanish Created function to download resume files deppending on language selected

--- src/components/home/SecondSection.js
+++ src/components/home/SecondSection.js
@@ -1,17 +1,27 @@
 import React from 'react';
 import { ReactSVG } from 'react-svg';
-// import { useTranslation } from 'react-i18next';
+import { useTranslation } from 'react-i18next';
 // Images
 import AVATAR from './../../assets/images/avatar.svg';
+// Documents
+import RESUME_EN from './../../assets/documents/ArielMolina-FrontendDeveloper.pdf';
+import RESUME_ES from './../../assets/documents/ArielMolinaCV.pdf';
 
 const SecondSection = () => {
 
-    // const { t } = useTranslation();
+    const { t, i18n } = useTranslation();
+
+    const lang = i18n.language;
+    const resumeUrl = lang === 'es' ? RESUME_ES : RESUME_EN;
 
     return (
-        <div id="second" className="row full-section second">
-            <div>
-            <ReactSVG className="avatar" src={AVATAR} />
+        // <ReactSVG className="avatar" src={AVATAR} />
+        <div id="second" className="row full-section second container mx-auto">
+            <div className="col-12 col-md-6 text-center">
+                <img src="https://dummyimage.com/400x600/000/fff" alt="Ariel about" />
+            </div>
+            <div className="col-12 col-md-6 info-container">
+                <a className="btn" href={resumeUrl} rel="noopener noreferrer" target="_blank">Download</a>
             </div>
         </div>
     );
