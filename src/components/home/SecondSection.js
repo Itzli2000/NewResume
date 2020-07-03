c272c33d85c97c75d54c683d9bb21caf45f58c52
Ariel  Molina <itzli2000@msn.com>
2020-07-03 11:33:01 -0500

    Refactor Working with structure of the second section of main page

--- src/components/home/SecondSection.js
+++ src/components/home/SecondSection.js
@@ -1,8 +1,10 @@
 import React from 'react';
-import { ReactSVG } from 'react-svg';
+// import { ReactSVG } from 'react-svg';
 import { useTranslation } from 'react-i18next';
 // Images
-import AVATAR from './../../assets/images/avatar.svg';
+// import AVATAR from './../../assets/images/avatar.svg';
+// Helpers
+import { calculate_age } from './../../helpers';
 // Documents
 import RESUME_EN from './../../assets/documents/ArielMolina-FrontendDeveloper.pdf';
 import RESUME_ES from './../../assets/documents/ArielMolinaCV.pdf';
@@ -13,6 +15,7 @@ const SecondSection = () => {
 
     const lang = i18n.language;
     const resumeUrl = lang === 'es' ? RESUME_ES : RESUME_EN;
+    const birth = new Date(1988, 5, 28);
 
     return (
         // <ReactSVG className="avatar" src={AVATAR} />
@@ -22,6 +25,23 @@ const SecondSection = () => {
                     <img src="https://dummyimage.com/400x600/000/fff" alt="Ariel about" />
                 </div>
                 <div className="col-12 col-lg-6 info-container">
+                    <h2>{t('STR_12')}</h2>
+                    <h3>{t('STR_13')}</h3>
+                    <p>{t('STR_14')}</p>
+                    <div className="info row">
+                        <div className="col-12 col-md-6">
+                            <p>{t('STR_15')} <span>{t('STR_15_1')}</span></p>
+                        </div>
+                        <div className="col-12 col-md-6">
+                            <p>{t('STR_16')} <span><a href={`tel:${t('STR_16_2')}`}>{t('STR_16_1')}</a></span></p>
+                        </div>
+                        <div className="col-12 col-md-6">
+                            <p>{t('STR_17')} <span>{calculate_age(birth)} {t('STR_17_1')}</span></p>
+                        </div>
+                        <div className="col-12 col-md-6">
+                            <p>{t('STR_18')} <span><a href={`mailto:${t('STR_18_1')}`}>{t('STR_18_1')}</a></span></p>
+                        </div>
+                    </div>
                     <a className="btn" href={resumeUrl} rel="noopener noreferrer" target="_blank">{t('STR_11')}</a>
                 </div>
             </div>
--- src/helpers.js
+++ src/helpers.js
@@ -1,40 +1,43 @@
-export function scrollPageTo (to, duration=500) {
-    //t = current time
-    //b = start value
-    //c = change in value
-    //d = duration
-    const easeInOutQuad = function (t, b, c, d) {
-      t /= d/2;
-      if (t < 1) return c/2*t*t + b;
-      t--;
-      return -c/2 * (t*(t-2) - 1) + b;
+export function scrollPageTo(to, duration = 500) {
+  //t = current time
+  //b = start value
+  //c = change in value
+  //d = duration
+  const easeInOutQuad = function (t, b, c, d) {
+    t /= d / 2;
+    if (t < 1) return c / 2 * t * t + b;
+    t--;
+    return -c / 2 * (t * (t - 2) - 1) + b;
+  };
+  return new Promise((resolve, reject) => {
+    const element = document.scrollingElement;
+    if (typeof to === 'string') {
+      to = document.querySelector(to) || reject();
+    }
+    if (typeof to !== 'number') {
+      to = to.getBoundingClientRect().top + element.scrollTop;
+    }
+    let start = element.scrollTop,
+      change = to - start,
+      currentTime = 0,
+      increment = 20;
+    const animateScroll = function () {
+      currentTime += increment;
+      let val = easeInOutQuad(currentTime, start, change, duration);
+      element.scrollTop = val;
+      if (currentTime < duration) {
+        setTimeout(animateScroll, increment);
+      } else {
+        resolve();
+      }
     };
-  
-    return new Promise((resolve, reject) => {
-      const element = document.scrollingElement;
-  
-      if (typeof to === 'string') {
-        to = document.querySelector(to) || reject();
-      }
-      if (typeof to !== 'number') {
-        to = to.getBoundingClientRect().top + element.scrollTop;
-      }
-  
-      let start = element.scrollTop,
-          change = to - start,
-          currentTime = 0,
-          increment = 20;
-  
-      const animateScroll = function() {
-          currentTime += increment;
-          let val = easeInOutQuad(currentTime, start, change, duration);
-          element.scrollTop = val;
-          if(currentTime < duration) {
-              setTimeout(animateScroll, increment);
-          } else {
-            resolve();
-          }
-      };
-      animateScroll();
-    });
-  }
+    animateScroll();
+  });
+}
+
+export function calculate_age(dob) { 
+  var diff_ms = Date.now() - dob.getTime();
+  var age_dt = new Date(diff_ms); 
+
+  return Math.abs(age_dt.getUTCFullYear() - 1970);
+}
