fba9b32300ee0218aeaa68a631866cec908d7b7c
Ariel  Molina <itzli2000@msn.com>
2020-07-03 20:07:24 -0500

    Updates

---
+++ precache-manifest.832f0b2ba78f572480773210003d51ff.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0efb2eb9083f93e42560b70f8e060d8c",
+    "url": "/Itzli2000/NewResume/index.html"
+  },
+  {
+    "revision": "b2cea86a0c7e62e5b7ec",
+    "url": "/Itzli2000/NewResume/static/css/2.e2dd9b0a.chunk.css"
+  },
+  {
+    "revision": "8d6dfd7391ad93258631",
+    "url": "/Itzli2000/NewResume/static/css/main.4a66e93d.chunk.css"
+  },
+  {
+    "revision": "b2cea86a0c7e62e5b7ec",
+    "url": "/Itzli2000/NewResume/static/js/2.a3f27840.chunk.js"
+  },
+  {
+    "revision": "113e2b2e4394d90cb544bfcfd93a5289",
+    "url": "/Itzli2000/NewResume/static/js/2.a3f27840.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8d6dfd7391ad93258631",
+    "url": "/Itzli2000/NewResume/static/js/main.d5122799.chunk.js"
+  },
+  {
+    "revision": "88daa41ad1238b4522ad",
+    "url": "/Itzli2000/NewResume/static/js/runtime-main.d8310b67.js"
+  },
+  {
+    "revision": "0fd8bf649db42778ec79bde49ed0a336",
+    "url": "/Itzli2000/NewResume/static/media/ArielMolina-FrontendDeveloper.0fd8bf64.pdf"
+  },
+  {
+    "revision": "ea311854c10863d1e9dc4cc7df840669",
+    "url": "/Itzli2000/NewResume/static/media/ArielMolinaCV.ea311854.pdf"
+  },
+  {
+    "revision": "f9013fbb8af907595f6b73a7910aef21",
+    "url": "/Itzli2000/NewResume/static/media/back.f9013fbb.svg"
+  },
+  {
+    "revision": "f32dfbe2ffc22352269e3843e45d2e67",
+    "url": "/Itzli2000/NewResume/static/media/bootstrap.f32dfbe2.svg"
+  },
+  {
+    "revision": "d44fa452ffb9c6868a81e96bf4792249",
+    "url": "/Itzli2000/NewResume/static/media/css3.d44fa452.svg"
+  },
+  {
+    "revision": "3b49c61c83f66cd9630b36d456508632",
+    "url": "/Itzli2000/NewResume/static/media/git.3b49c61c.svg"
+  },
+  {
+    "revision": "831893dc3ba87c86a51f5f23d95445b9",
+    "url": "/Itzli2000/NewResume/static/media/github.831893dc.svg"
+  },
+  {
+    "revision": "26915afd8ece74abd23b401a0a518003",
+    "url": "/Itzli2000/NewResume/static/media/js.26915afd.svg"
+  },
+  {
+    "revision": "4fb7c48ff03d98d3566d691cb6eec67e",
+    "url": "/Itzli2000/NewResume/static/media/mx.4fb7c48f.svg"
+  },
+  {
+    "revision": "668eb6532b441f69728e00b8985808c6",
+    "url": "/Itzli2000/NewResume/static/media/sass.668eb653.svg"
+  },
+  {
+    "revision": "50162f60f2cd5ba81c94bbd8ec32dee0",
+    "url": "/Itzli2000/NewResume/static/media/us.50162f60.svg"
+  }
+]);
