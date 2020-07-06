3f8a2740828f66f5f0862866547c0af5aeb3451f
Ariel  Molina <itzli2000@msn.com>
2020-07-05 21:08:01 -0500

    Updates

---
+++ precache-manifest.ba312fa356b0cd5cea7c031422cf00c9.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6aaf3a3c7e1632895f7edf279905a9ca",
+    "url": "/NewResume/index.html"
+  },
+  {
+    "revision": "34216079fe785624b818",
+    "url": "/NewResume/static/css/2.e2dd9b0a.chunk.css"
+  },
+  {
+    "revision": "88bad506546318d397e5",
+    "url": "/NewResume/static/css/main.d45e5ffd.chunk.css"
+  },
+  {
+    "revision": "34216079fe785624b818",
+    "url": "/NewResume/static/js/2.89fcd053.chunk.js"
+  },
+  {
+    "revision": "113e2b2e4394d90cb544bfcfd93a5289",
+    "url": "/NewResume/static/js/2.89fcd053.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "88bad506546318d397e5",
+    "url": "/NewResume/static/js/main.d640cec4.chunk.js"
+  },
+  {
+    "revision": "fdad196bea653af8c1e5",
+    "url": "/NewResume/static/js/runtime-main.2aaaf4e4.js"
+  },
+  {
+    "revision": "0fd8bf649db42778ec79bde49ed0a336",
+    "url": "/NewResume/static/media/ArielMolina-FrontendDeveloper.0fd8bf64.pdf"
+  },
+  {
+    "revision": "ea311854c10863d1e9dc4cc7df840669",
+    "url": "/NewResume/static/media/ArielMolinaCV.ea311854.pdf"
+  },
+  {
+    "revision": "f9013fbb8af907595f6b73a7910aef21",
+    "url": "/NewResume/static/media/back.f9013fbb.svg"
+  },
+  {
+    "revision": "f32dfbe2ffc22352269e3843e45d2e67",
+    "url": "/NewResume/static/media/bootstrap.f32dfbe2.svg"
+  },
+  {
+    "revision": "3b49c61c83f66cd9630b36d456508632",
+    "url": "/NewResume/static/media/git.3b49c61c.svg"
+  },
+  {
+    "revision": "831893dc3ba87c86a51f5f23d95445b9",
+    "url": "/NewResume/static/media/github.831893dc.svg"
+  },
+  {
+    "revision": "26915afd8ece74abd23b401a0a518003",
+    "url": "/NewResume/static/media/js.26915afd.svg"
+  },
+  {
+    "revision": "4fb7c48ff03d98d3566d691cb6eec67e",
+    "url": "/NewResume/static/media/mx.4fb7c48f.svg"
+  },
+  {
+    "revision": "7a79f9bade78a7980daff74a360da363",
+    "url": "/NewResume/static/media/p1.7a79f9ba.png"
+  },
+  {
+    "revision": "6519529ea66490aaa29d36fc026efd9d",
+    "url": "/NewResume/static/media/p2.6519529e.png"
+  },
+  {
+    "revision": "3cb4921fca34e3fa56abf19e2bab3161",
+    "url": "/NewResume/static/media/p3.3cb4921f.png"
+  },
+  {
+    "revision": "7b1db387c913167ba4a79d5b5720048b",
+    "url": "/NewResume/static/media/p4.7b1db387.png"
+  },
+  {
+    "revision": "1ac3c7533aae9fd7ca78a8b2f3bcaa65",
+    "url": "/NewResume/static/media/p5.1ac3c753.png"
+  },
+  {
+    "revision": "e2e0226fa6f186dd2282d78ad6fd2344",
+    "url": "/NewResume/static/media/p6.e2e0226f.png"
+  },
+  {
+    "revision": "794fec23b879f24f84c85e3e05f8d44e",
+    "url": "/NewResume/static/media/p7.794fec23.png"
+  },
+  {
+    "revision": "93856422b413e66a4c7dd9cfa08b08fe",
+    "url": "/NewResume/static/media/react.93856422.svg"
+  },
+  {
+    "revision": "668eb6532b441f69728e00b8985808c6",
+    "url": "/NewResume/static/media/sass.668eb653.svg"
+  },
+  {
+    "revision": "50162f60f2cd5ba81c94bbd8ec32dee0",
+    "url": "/NewResume/static/media/us.50162f60.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/NewResume/precache-manifest.bbaa58d730c1ca414185da0dfa93960e.js"
+  "/NewResume/precache-manifest.ba312fa356b0cd5cea7c031422cf00c9.js"
 );
 
 self.addEventListener('message', (event) => {
